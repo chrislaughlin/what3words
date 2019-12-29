c5579271818207c29be336625ca40b659ee9494a
Chris Laughlin <chris_laughlin@rapid7.com>
2019-12-29 21:02:35 +0000

    basiv working version

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
+/* eslint-disable no-undef */
+import React, {useState, useEffect} from 'react';
+import styled from 'styled-components';
+
 import './App.css';
 
+const get3Words = async (latitude, longitude) => {
+    const currentLocation = await what3words.api.convertTo3wa({lat:latitude, lng:longitude})
+    const southwest = await what3words.api.convertTo3wa(currentLocation.square.southwest);
+    const northeast = await what3words.api.convertTo3wa(currentLocation.square.northeast);
+
+    return {
+        currentLocation,
+        southwest,
+        northeast
+    };
+}
+
+const Square = styled.p`
+    border: 10px dashed #817c7c;
+    padding: 20% 0%;
+`;
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [words, setWords] = useState('Getting Location...');
+
+    useEffect(() => {
+      if (!navigator.geolocation) {
+        setWords('Geolocation is not supported by your browser');
+      } else {
+          navigator.geolocation.getCurrentPosition(
+              pos => {
+                  get3Words(pos.coords.latitude, pos.coords.longitude)
+                      .then(data => setWords(data.currentLocation.words));
+
+              },
+              error => setWords('Geolocation is needed to get your 3 words')
+          );
+      }
+    }, [])
+
+    return (
+        <div>
+            <Square>
+              {words}
+            </Square>
+        </div>
+    );
 }
 
 export default App;
